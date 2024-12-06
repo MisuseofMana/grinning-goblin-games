ca32ce127b7e81f9cfc57ea20dd9318b1b9d62f3
Sean Yager <contact@seanyager.com>
2024-12-05 19:18:12 -0500

    expanding on deck building, starting shader

--- components/cards/card_setup.gd
+++ components/cards/card_setup.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var scene_base = $"."
 @onready var rich_card_description: RichTextLabel = $RichCardDescription
 @onready var card_image_slot: TextureRect = $CardImageSlot
 @onready var card_name: Label = $CardName
@@ -7,6 +8,10 @@ extends Control
 @onready var card_base: TextureRect = $CardBase
 
 @export var card_stats : CardStats
+@export var isEditingDeck : bool = false
+
+signal card_added_to_deck(card)
+signal card_removed_from_deck(card)
 
 const PLAYER = preload("res://components/units/UnitDictionary/UnitTypes/player.tres")
 
@@ -15,3 +20,13 @@ func setCardData():
 	rich_card_description.text = card_stats.card_description % GameLogic.calculateCardCost(card_stats, PLAYER, false)
 	card_image_slot.texture = card_stats.card_image 
 	card_cost_label.text = str(card_stats.play_cost)
+
+func _card_display_gui_input(event):
+	if not isEditingDeck:
+		return
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			card_added_to_deck.emit(self)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+		if event.is_pressed():
+			card_removed_from_deck.emit(self)
--- scenes/deck_management.gd
+++ scenes/deck_management.gd
@@ -1,28 +1,60 @@
 extends Node2D
 class_name DeckManagement
 
-@onready var library: ScrollContainer = $Library
-@onready var library_grid: GridContainer = $Library/LibraryGrid
+@onready var library_grid: GridContainer = $LibraryView/Library/LibraryGrid
+@onready var deck_grid : GridContainer = $DeckView/Deck/DeckGrid
 
 @export var cardsOwned : Array[CardStats] = []
-@export var deckCards : Array[CardStats] = []
+@export var deckCards : Array[Control] = []
+
+@onready var anims = $AnimationPlayer
 
 const CARD_DISPLAY = preload("res://components/cards/card_display.tscn")
 const DETECTION_AREA_CARD = preload("res://components/cards/detection_area_card.tscn")
 
+var current_view : String = 'library'
+
 func _ready() -> void:
+#	remove all cards in grid to ensure no lingering development cards exist
 	for dummyCard in library_grid.get_children():
 		dummyCard.queue_free()
+	for dummyCard in deck_grid.get_children():
+		dummyCard.queue_free()
+		
 	for card in cardsOwned:
 		var newCard = CARD_DISPLAY.instantiate()
 		newCard.card_stats = card
 		library_grid.add_child(newCard)
 		newCard.setCardData()
-		var newArea = DETECTION_AREA_CARD.instantiate()
-		newCard.add_child(newArea)
+		#var newArea = DETECTION_AREA_CARD.instantiate()
+		#newCard.add_child(newArea)
+		newCard.isEditingDeck = true
+		newCard.card_added_to_deck.connect(addCardToDeck)
+		newCard.card_removed_from_deck.connect(removeCardFromDeck)
+		
+	#for card in deckCards:
+		#var newCard = CARD_DISPLAY.instantiate()
+		#newCard.card_stats = card
+		#deck_grid.add_child(newCard)
+		#newCard.setCardData()
+		#var newArea = DETECTION_AREA_CARD.instantiate()
+		#newCard.add_child(newArea)
+		#newCard.add_to_discard_number.connect(addCardToDeck)
 
-func addCardToDeck():
-	pass
+func addCardToDeck(card):
+	deckCards.append(card)
+	card.
+	print(deckCards)
 	
-func removeCardFromDeck():
-	pass
+func removeCardFromDeck(card):
+	deckCards.erase(card)
+	print(deckCards)
+
+func outlineCard():
+	for card in library_grid.get_children():
+
+func viewLibrary():
+	anims.play("go_to_library")
+	
+func viewDeck():
+	anims.play('go_to_deck')
